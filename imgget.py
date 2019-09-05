f42f51fb0dba2bd3673cbc9edb052d31520c1013
cphil124 <47870744+VCG-cphillips@users.noreply.github.com>
2019-09-05 12:09:33 -0500

    Added file path utility functions

--- imgget.py
+++ imgget.py
@@ -3,6 +3,7 @@ from io import open as iopen
 import os
 import torch
 from urllib.parse import urlsplit
+import pathlib
 
 def requests_image(file_url):
     suffix_list = ['jpg', 'png', 'gif']
@@ -29,13 +30,36 @@ def get_url_filename(file_url):
     return urlsplit(file_url)[2].split('/')[-1]
 
 def get_img_normalize_mean(device='cpu'):
+    """
+    Returns a tensor containing the standard normal distribution's mean for image colorpaths.
+    Intended for use in normalization image data for use in Machine/Deep Learning
+    """
     if device == 'cpu':
         return torch.tensor([0.485, 0.456, 0.406])
     elif device == 'cuda':
         return torch.tensor([0.485, 0.456, 0.406]).to(device)
 
 def get_img_normalize_std(device='cpu'):
+    """
+    Returns a tensor containing the standard normal distribution's standard deviation for image colorpaths
+    Intended for use in normalization image data for use in Machine/Deep Learning
+    """
     if device == 'cpu':
         return torch.tensor([0.229, 0.224, 0.225])
     elif device == 'cuda':
         return torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+def get_path_filename(file_path):
+    """
+    Return the name of the file or directory when passed an absolute or partial path
+    """
+    path = pathlib.PurePath(file_path)
+    return path.name
+
+def get_path_file_title(file_path):
+    """
+    Return the title of the file a path is pointing to. 
+    The title is the name of the file, minus the filetype extension
+    """
+    path = pathlib.PurePath(file_path)
+    return path.name.split('.')[0]
